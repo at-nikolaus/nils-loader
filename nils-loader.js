9a653514737e254a52d2d4fa1599f50ea16e6801
Frank Lemanschik <frank@github.dspeed.eu>
2017-11-04 02:21:31 +0000

    Update nils-loader.js

--- nils-loader.js
+++ nils-loader.js
@@ -1,15 +1,17 @@
-class nils
-
-export default function loadScript(src) {
- return new Promise(function(resolve, reject) {
-   const script = document.createElement('script');
-   script.async = true;
-   script.src = src;
-   script.onload = resolve;
-   script.onerror = reject;
-   document.head.appendChild(script);
- });
+export default class nilsLoader extends HTMLElement {
+ loadScript(src) {
+  return new Promise(function(resolve, reject) {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+ }
 }
+
+export default function 
 /*
 usage like
 import nils-loader.js 
