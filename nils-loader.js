96433f8cccbd4afaf6fa927699fd3720a052e189
Frank Lemanschik <frank@github.dspeed.eu>
2017-11-04 01:03:12 +0000

    Create nils-loader.js

---
+++ nils-loader.js
@@ -0,0 +1,17 @@
+class nils
+
+export default function loadScript(src) {
+ return new Promise(function(resolve, reject) {
+   const script = document.createElement('script');
+   script.async = true;
+   script.src = src;
+   script.onload = resolve;
+   script.onerror = reject;
+   document.head.appendChild(script);
+ });
+}
+/*
+usage like
+import nils-loader.js 
+loadScript('my/script.js').then(
+*/
